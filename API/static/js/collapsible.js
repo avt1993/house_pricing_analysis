062752a42bbd738a1b7992495fcfff9b4fbfadf0
Antonio Villardaga <116745283+avt1993@users.noreply.github.com>
2023-06-21 23:25:06 -0700

    Add files via upload

---
+++ API/static/js/collapsible.js
@@ -0,0 +1,20 @@
+function collapsible(section){
+    let sections=["filters","graphs","predict"].filter(each=>{
+        return each!=section;
+    });
+    console.log("Remaining sections",sections);
+    if (d3.select(`#${section}`).style("width")=="0px"){
+        console.log(`expand ${section}`)
+        sections.forEach(each=>{
+            d3.select(`#${each}`).style("width",0);
+            d3.select(`#${each}`).style("marginLeft",0);
+        });
+        d3.select(`#${section}`).style("width","450px");
+        d3.select(`#${section}`).style("marginLeft","20px");
+
+    } else {
+        console.log(`collapse ${section}`)
+        d3.select(`#${section}`).style("width",0);
+        d3.select(`#${section}`).style("marginLeft",0);
+    }
+}
